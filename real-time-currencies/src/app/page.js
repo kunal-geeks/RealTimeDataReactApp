e3ab34181c803a801795deec4270a5bbbdd4921d
Kunal Sharma <113223333+kunal-geeks@users.noreply.github.com>
2023-08-02 13:20:01 +0000

    learned fetching data

--- real-time-currencies/src/app/page.js
+++ real-time-currencies/src/app/page.js
@@ -5,12 +5,14 @@ import { useState, useEffect } from 'react'
 
 export default function Home() {
   const [pokeData,setPokeData] = useState({})
+  const [pokemonImage,setPokemonImage] = useState('')
   useEffect(()=>{
     async function getInfo() {
       try {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
         const jsonData = await response.json();
         setPokeData(jsonData)
+        setPokemonImage(jsonData.sprites.front_default)
       }catch(error) {
         console.error(error)
       }
@@ -25,14 +27,14 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello World!</h1>
-      {pokeData.sprites && (
-        <Image
-          src={pokeData.sprites.front_default}
-          alt={pokeData.name}
-          width={100}
-          height={100}
-        />
-      )}
+      {pokemonImage && pokeData ?
+        <>
+          <img src={pokemonImage} alt={pokeData.name} width={200} height={200} />
+          <h2>{pokeData.name}</h2>
+        </>
+        :
+        <p></p>
+      }
     </main>
   )
 }
